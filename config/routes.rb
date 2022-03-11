e76dfbeffd25c2e01af5121fddf61eae4be3e706
kentaro425 <95513931+kentaro425@users.noreply.github.com>
2022-03-11 12:59:13 +0000

    [add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,67 +1,78 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'notifications/index'
-  end
-  namespace :public do
-    get 'relationships/followings'
-    get 'relationships/followers'
-  end
-  namespace :public do
-    get 'posts/new'
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/edit'
-    get 'posts/search'
-  end
-  namespace :public do
-    get 'rooms/index'
-  end
-  namespace :public do
-    get 'chats/show'
-  end
-  namespace :public do
-    get 'requests/new'
-    get 'requests/index'
-    get 'requests/show'
-    get 'requests/edit'
-    get 'requests/search'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/requester_show'
-    get 'users/sns_show'
-    get 'users/edit'
-    get 'users/requester_edit'
-    get 'users/sns_edit'
-    get 'users/unsubscribe'
-    get 'users/search'
-    get 'users/favorites'
-    get 'users/sns_favorites'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :admin do
-    get 'notifications/index'
-  end
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-  end
-  # devise_for :admins
-  # devise_for :users
-  # 顧客用
+    # 顧客用
   # URL /customers/sign_in ...
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
+  scope module: :public do
+    resources :users, only: [:edit, :update] do
+      member do
+        get 'home', to: 'users#show'
+        get 'requester_home', to: 'users#requester_show'
+        get 'sns_home', to: 'users#sns_show'
+        get 'requester_edit'
+        get 'sns_edit'
+        get 'unsubscribe'
+        patch 'withdraw'
+        get 'favorites'
+        get 'sns_favorites'
+      end
+
+      collection do
+        get 'search'
+      end
+
+      resources :relationships, only: [:create, :destroy] do
+        collection do
+          get 'followings'
+          get 'followers'
+        end
+      end
+
+      resources :notifications, only: [:index, :destroy] do
+        collection do
+          delete "destroy_all"
+        end
+      end
+    end
+
+    resources :requests do
+      collection do
+        get 'search'
+      end
+      resources :favorites, only: [:create, :destroy]
+    end
+
+    resources :chats, only: [:show, :create]
+
+    get 'rooms/index'
+    # root 'homes/top'
+    get 'homes/about', as: 'about'
+
+    resources :posts do
+      collection do
+        get 'search'
+      end
+      resources :favorites, only: [:create, :destroy]
+      resources :comments, only: [:create]
+    end
+  end
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :notifications, only: [:index, :destroy] do
+      collection do
+        delete "destroy_all"
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
