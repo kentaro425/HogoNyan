9480490c07dd37affcab94bf3b7a32cee3a05105
kentaro425 <95513931+kentaro425@users.noreply.github.com>
2022-03-13 04:57:50 +0000

    [add]homeページ

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,4 +59,15 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def after_sign_in_path_for(resource)
+    home_user_path
+  end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :nickname, :postal_code, :address, :phone])
+  end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,6 @@
 class Public::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :requester_show, :sns_show, :edit, :update]
+
   def show
   end
 
@@ -16,13 +18,20 @@ class Public::UsersController < ApplicationController
 
   def sns_edit
   end
-  
+
   def update
+    if @user.update(user_params)
+      flash[:notice] = "ユーザ情報を変更しました。"
+      redirect_to home_user_path
+    else
+      render :edit
+    end
+
   end
 
   def unsubscribe
   end
-  
+
   def withdraw
   end
 
@@ -33,5 +42,15 @@ class Public::UsersController < ApplicationController
   end
 
   def sns_favorites
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :phone, :nickname, :profile, :introduction, :status)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
