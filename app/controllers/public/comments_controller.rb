2312ea0255954fb951964491c87ab78e9e399c05
kentaro425 <95513931+kentaro425@users.noreply.github.com>
2022-03-18 14:27:11 +0000

    [add]コメント機能

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,5 +1,19 @@
 class Public::CommentsController < ApplicationController
-  
-def create
-end
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment.post_id = @post.id
+    # @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to post_path(@post), notice: 'コメントを投稿しました'
+    else
+      render 'posts/show'
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :post_id, :user_id)
+  end
+
 end
