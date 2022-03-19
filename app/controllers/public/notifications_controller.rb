0419621ae889c7ba46488e767dcda5124986599b
kentaro425 <95513931+kentaro425@users.noreply.github.com>
2022-03-19 12:51:28 +0000

    [add]通知機能

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,10 +1,20 @@
 class Public::NotificationsController < ApplicationController
   def index
+    notifications = current_user.passive_notifications.page(params[:page]).per(20)
+    @notifications = notifications.where(checked: false)
   end
-  
+
   def destroy
+    @notification = Notification.find(params[:id])
+    @notification.update(checked: true)
+    redirect_to request.referer
   end
-  
+
   def destroy_all
+    @notifications = current_user.passive_notifications.page(params[:page]).per(20)
+    @notifications.where(checked: false).each do |notification|
+      notification.update(checked: true)
+    end
+    redirect_to request.referer
   end
 end
