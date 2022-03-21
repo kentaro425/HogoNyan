e40856d6db327d4ee5b9ffb19b34a763e412cdd5
kentaro425 <95513931+kentaro425@users.noreply.github.com>
2022-03-21 10:48:14 +0000

    [Fix] Migration

--- db/migrate/20220313135338_drop_table_prefectures.rb
+++
@@ -1,9 +0,0 @@
-class DropTablePrefectures < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :prefectures do |t|
-    t.integer :region_id, null: false
-    t.integer :name, default: 0, null: false
-    t.timestamps null: false
-  end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_13_135338) do
+ActiveRecord::Schema.define(version: 2022_03_13_074333) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
